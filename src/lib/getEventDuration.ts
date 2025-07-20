16526d2d2f63bf8ebb152948700e9a59c2ddaaa5
Jakob Listabarth <76778035+jakoblistabarth@users.noreply.github.com>
2025-07-20 14:23:09 +0200

    :recycle: Make timezone dynamic

--- src/lib/getEventDuration.ts
+++ src/lib/getEventDuration.ts
@@ -4,12 +4,13 @@ type EventDateTime = Awaited<
   ReturnType<typeof getCollection<"events">>
 >[number]["data"]["dateTime"];
 
-export const getEventDuration = (eventDateTime: EventDateTime) => {
+export const getEventDuration = (
+  eventDateTime: EventDateTime,
+  timeZone: string
+) => {
   if (!eventDateTime) {
     throw new Error("No date available");
   }
-
-  const timeZone = "PST"; // Pacific Standard Time
 
   const [start, end] = Object.values(eventDateTime).map((d) => new Date(d));
   const startString = start.toLocaleString(undefined, {
