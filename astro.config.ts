97d84ed2b89c3f0e000607ef7345162f073a7291
Jakob Listabarth <76778035+jakoblistabarth@users.noreply.github.com>
2026-01-07 14:40:24 +0100

    :construction: Add author and tutorial collection

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://next-generation-cartographers.github.io/",
+  integrations: [mdx()],
 });
