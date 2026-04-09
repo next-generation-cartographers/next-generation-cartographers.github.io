27c6983e5074a316f720f76b625dfee0ead64039
Jakob Listabarth <76778035+jakoblistabarth@users.noreply.github.com>
2026-04-09 13:07:04 +0200

    :adhesive_bandage: Fix urls for social links
    Also add icons via astro-icons

--- astro.config.ts
+++ astro.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://next-generation-cartographers.github.io/",
-  integrations: [mdx()],
+  integrations: [mdx(), icon()],
 });
